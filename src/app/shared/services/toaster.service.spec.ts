5120f5fc73e1950bb5e232e370c6dd24510b1bfd
puneetravinder <145984010+puneetravinder@users.noreply.github.com>
2024-02-02 19:12:28 +0530

    Test cases changes

--- src/app/shared/services/toaster.service.spec.ts
+++ src/app/shared/services/toaster.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ToasterService } from './toaster.service';
 import { MessageService } from 'primeng/api';
 
@@ -11,9 +10,33 @@ describe('ToasterService', () => {
       providers: [MessageService]
     });
     service = TestBed.inject(ToasterService);
-  });
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-});
+  })
+
+  it('should call success method', () => {
+    let spy = spyOn(service, 'success');
+    service.success('Successfully called', 'success');
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call error method', () => {
+    let spy = spyOn(service, 'error');
+    service.error('Error found', 'error');
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call info method', () => {
+    let spy = spyOn(service, 'info');
+    service.info('Important information', 'info');
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call warning method', () => {
+    let spy = spyOn(service, 'warning');
+    service.warning('Warning alert', 'warning');
+    expect(spy).toHaveBeenCalled();
+  })
+})
