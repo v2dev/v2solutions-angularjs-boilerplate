c9ad2321514f0d90e7ebd529f86aecc936d426a4
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-02-14 18:17:09 +0530

    Testing terragrunt destroy

--- scripts/delete-objects.sh
+++ scripts/delete-objects.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Check if DeleteMarkers[] is present
+echo "Starting delete script"
+
 if aws s3api list-object-versions --bucket v2-angularjs-boilerplate --query 'DeleteMarkers' >/dev/null 2>&1; then
     echo "DeleteMarkers[] found, executing delete-objects for DeleteMarkers"
     aws s3api delete-objects --bucket v2-angularjs-boilerplate --delete "$(aws s3api list-object-versions --bucket v2-angularjs-boilerplate --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')"
